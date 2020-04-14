33c51a6cca5d3da6fbf902eddada55c3267c5753
Marden Cavalcante <marden.ferreira@synapsebrasil.com.br>
2020-04-13 23:40:16 -0300

    Update in default values

--- js/script.js
+++ js/script.js
@@ -7,39 +7,51 @@ $(function () {
     let regimeSN = $('#regime_sn');
     let typeEnterpriseCommerce = $('#commerce');
     let typeEnterpriseService = $('#service');
+    let totalEconomy = $('#total-economy');
 
     // Load initial value with default data
-    calculateMyParcel();
+    updateMyHtml();
 
     // Setting my listeners to check changes on values
-    quantityEmployers.on('keyup', () => calculateMyParcel());
-    regimeLP.change(() => calculateMyParcel());
-    regimeSN.change(() => calculateMyParcel());
-    typeEnterpriseCommerce.change(() => calculateMyParcel());
-    typeEnterpriseService.change(() => calculateMyParcel());
+    quantityEmployers.on('keyup', () => updateMyHtml());
+    regimeLP.change(() => updateMyHtml());
+    regimeSN.change(() => updateMyHtml());
+    typeEnterpriseCommerce.change(() => updateMyHtml());
+    typeEnterpriseService.change(() => updateMyHtml());
 
-    function checkTypeRegime() {
+    function checkTypeRegime(commerce = 300, otherValue = 129) {
         let val = 0;
-        typeEnterpriseCommerce.is(':checked') ? val = 300 : val = 129;
+        typeEnterpriseCommerce.is(':checked') ? val = commerce : val = otherValue;
         return val;
     }
 
-    function checkTypeEnterprise() {
+    function checkTypeEnterprise(regLP = 200, otherValue = 0) {
         let val = 0;
-        regimeLP.is(':checked') ? val = 200 : val;
+        regimeLP.is(':checked') ? val = regLP : val = otherValue;
         return val;
     }
 
-    function calculateEmployers(employers) {
-        return employers * 50;
+    function calculateEmployers(employers, priceEmployers = 50) {
+        return employers * priceEmployers;
     }
 
     function calculateMyParcel() {
         let me = quantityEmployers.val();
         let total = checkTypeRegime() + checkTypeEnterprise() + calculateEmployers(me) + parseInt(monthlyParcel.val());
         let monthly = total / 12;
-        let myTotal = monthly.toLocaleString('pt-BR', {minimumFractionDigits: 2, maximumFractionDigits: 2});
-        console.log(myTotal);
-        showTotal.html('R$ '+ myTotal);
+        return monthly.toLocaleString('pt-BR', {minimumFractionDigits: 2, maximumFractionDigits: 2});
+    }
+
+    function calculateMyParcelOlder() {
+        let me = quantityEmployers.val();
+        let total = checkTypeRegime(500, 200) + checkTypeEnterprise() + calculateEmployers(me) + parseInt(monthlyParcel.val());
+        return total.toLocaleString('pt-BR', {minimumFractionDigits: 2, maximumFractionDigits: 3});
+    }
+
+    function updateMyHtml() {
+        showTotal.html('R$ ' + calculateMyParcel());
+        let aux = parseInt(calculateMyParcelOlder()) - (12 * parseInt(calculateMyParcel()));
+        aux <= 0 ? aux = 0 : aux;
+        totalEconomy.html('R$ ' + aux);
     }
 });
