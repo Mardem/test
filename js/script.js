3f00459577c3dde358849fe869a166c5129743b9
Marden Cavalcante <marden.ferreira@synapsebrasil.com.br>
2020-04-13 23:19:43 -0300

    Atualização no cálculo de valores

--- js/script.js
+++ js/script.js
@@ -1,56 +1,45 @@
-var qtdeFuncionarios, qtdeProLabores, qtdeDocsContabeis, qtdeDocsFiscais, minPrice, maxPrice, maxDocsFiscais,
-    maxDocsContabeis, maxProLabores, planos, sn_lp;
 $(function () {
-    $.get('http://localhost:3000/data').done(function (data, textStatus, jqXHR) {
-        planos = data.planos;
-        maxDocsFiscais = parseInt(data.maxDocsFiscais);
-        maxPrice = parseFloat(data.maxPrice);
-        gatherDataAndSimulate();
-    }).fail(function (jqXHR, textStatus, errorThrown) {
-    });
-    $('[name*="qtde_funcionario"]').val(0);
-    $('[name*="qtde_documento_fiscal"]').val(10);
-    $('#contabilidade').val('499,99')
-    $('[name="regime"]').on('change', function () {
-        gatherDataAndSimulate();
-    });
-    $('#contabilidade, [name*="qtde_funcionario"], [name*="qtde_pro_labores"],[name*="qtde_documento_contabil"],[name*="qtde_documento_fiscal"]').on('blur, focus, keyup', function () {
-        gatherDataAndSimulate();
-    });
+    // Select us input with variables
+    let quantityEmployers = $('[name="quantity-employers"]');
+    let showTotal = $('#per-monthly');
+    let monthlyParcel = $('#enterprise');
+    let regimeLP = $('#regime_lp');
+    let regimeSN = $('#regime_sn');
+    let typeEnterpriseCommerce = $('#commerce');
+    let typeEnterpriseService = $('#service');
+
+    // Load initial value with default data
+    calculateMyParcel();
+
+    // Setting my listeners to check changes on values
+    quantityEmployers.on('keyup', () => calculateMyParcel());
+    regimeLP.change(() => calculateMyParcel());
+    regimeSN.change(() => calculateMyParcel());
+    typeEnterpriseCommerce.change(() => calculateMyParcel());
+    typeEnterpriseService.change(() => calculateMyParcel());
+
+    function checkTypeRegime() {
+        let val = 0;
+        typeEnterpriseCommerce.is(':checked') ? val = 300 : val = 129;
+        return val;
+    }
+
+    function checkTypeEnterprise() {
+        let val = 0;
+        regimeLP.is(':checked') ? val = 200 : val;
+        return val;
+    }
+
+    function calculateEmployers(employers) {
+        return employers * 50;
+    }
+
+    function calculateMyParcel() {
+        let me = quantityEmployers.val();
+        let total = checkTypeRegime() + checkTypeEnterprise() + calculateEmployers(me) + parseInt(monthlyParcel.val());
+        let monthly = total / 12;
+        let myTotal = monthly.toLocaleString('pt-BR', {minimumFractionDigits: 2, maximumFractionDigits: 2});
+        console.log(myTotal);
+        showTotal.html('R$ '+ myTotal);
+    }
 });
-
-function gatherDataAndSimulate() {
-    $('#modal-simulate .modal-body').empty();
-    qtdeFuncionarios = isNaN(parseInt($('[name*="qtde_funcionario"]').val())) ? 0 : $('[name*="qtde_funcionario"]').val();
-    qtdeDocsFiscais = isNaN(parseInt($('[name*="qtde_documento_fiscal"]').val())) ? 0 : $('[name*="qtde_documento_fiscal"]').val();
-    simulateMonthlyPayment(qtdeFuncionarios, qtdeDocsFiscais);
-}
-
-function simulateMonthlyPayment(qtdeFuncionarios, qtdeDocFiscais) {
-    var acrescimoFuncionarios = 0;
-    minPrice = maxPrice;
-    $('#regime_lp').prop('checked') == true ? sn_lp = 200 : sn_lp = 0;
-    if (qtdeDocFiscais > maxDocsFiscais) {
-        $('[name*="qtde_documento_fiscal"]').val(maxDocsFiscais);
-    }
-    if (qtdeFuncionarios >= 10) {
-        acrescimoFuncionarios = qtdeFuncionarios * 20;
-    } else {
-        acrescimoFuncionarios = qtdeFuncionarios * 25;
-    }
-    for (var i in planos) {
-        if (qtdeDocFiscais <= parseInt(planos[i].total_documento_fiscal) && parseFloat(planos[i].valor) < minPrice) {
-            minPrice = parseFloat(planos[i].valor);
-        }
-    }
-    minPrice += sn_lp;
-    minPrice = parseFloat(minPrice + acrescimoFuncionarios).toFixed(2);
-    var contabilidade = $('#contabilidade').val().replace(".", "");
-    contabilidade = parseFloat(contabilidade.replace(",", "."));
-    var totalDesconto = (contabilidade * 12) - (minPrice * 12) > 0 ? (contabilidade * 12) - (minPrice * 12) : 0;
-    minPrice = minPrice.replace(".", ",");
-    totalDesconto = totalDesconto.toFixed(2).replace(".", ",");
-    $('#valor-mensalidade span').text(minPrice);
-    $('#valor-economia span').text(totalDesconto);
-    $('#modal-simulate .modal-body').html($('#mensalidade-box').html())
-}
